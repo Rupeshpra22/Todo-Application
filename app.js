1102d031e4b2710f2bc1478e30b046dd15188fcb
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2020-09-07 20:27:52 +0530

    Storing todolist in localStorage and some cosmetic changes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Functions
-addTodo = (event) => {
+
+
+addTodo = (event, todo) => {
     //Prevent form from submitting
     event.preventDefault();
 
@@ -14,16 +16,19 @@ addTodo = (event) => {
 
     //Create li
     const newTodo = document.createElement('li');
-    if (todoInput.value != "") {
-        newTodo.innerText = todoInput.value;
-        newTodo.title = todoInput.value;
+    if (todoInput.value != "" || todo) {
+        newTodo.innerText = todo ? todo : todoInput.value;
+        newTodo.title = todo ? todo : todoInput.value;
         //ways of giving text to a element
         //newTodo.innerHTML="hey"
         //newTodo.append(document.createTextNode('hey'))
         newTodo.classList.add('todo-item');
         todoDiv.appendChild(newTodo);
-        todoInput.value = ""
 
+        //Add Todo to localStorage
+        if (todo === undefined) {
+            saveLocalTodos(todoInput.value);
+        }
 
         //Check mark button
         const completedButton = document.createElement('button');
@@ -42,6 +47,7 @@ addTodo = (event) => {
 
         //Appending todoDiv to ul
         todoList.appendChild(todoDiv);
+        todoInput.value = ""
     }
 }
 
@@ -63,6 +69,7 @@ deleteCheck = (event) => {
         //we just sets the opacity to 0
         //todo.classList.add('fall');
         todo.classList.add('fall');
+        removeLocalStorageTodos(todo);
         //so we have to remove the todo list as soon as the transition
         //ends, for that we have special eventListener called
         //transitionend(for transition), we also have animationend(for animation)
@@ -70,6 +77,8 @@ deleteCheck = (event) => {
         todo.addEventListener('transitionend', () => {
             todo.remove(); //to remove an element
         })
+
+
     }
 
     //CHECKMARK
@@ -79,8 +88,72 @@ deleteCheck = (event) => {
     }
 }
 
+filterTodo = (event) => {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (event.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "Completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none'
+                }
+                break;
+            case "Uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none'
+                }
+                break;
+        }
+    })
+}
+
+getTodosFromLocalStorage = () => {
+    let todoList;
+    if (localStorage.getItem('todos') === "") {
+        todoList = [];
+        return todoList;
+    } else {
+        //if todo is already present get the todos from localStorage
+        todoList = JSON.parse(localStorage.getItem('todos'));
+        return todoList;
+    }
+}
+//Storing Todolist in LocalStorage
+saveLocalTodos = (todo) => {
+    let todos = [];
+    todos = getTodosFromLocalStorage();
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+
+getTodos = () => {
+    let todos = getTodosFromLocalStorage();
+    if(todos.length>0){
+        todos.forEach((todo) => {
+            addTodo(event, todo);
+        })
+    }
+}
+
+removeLocalStorageTodos = (todo) =>{
+    let todos = getTodosFromLocalStorage();
+    let todoName = todo.children[0].innerText;
+    let indexOfTodo = todos.indexOf(todoName);
+    todos.splice(indexOfTodo,1);
+    localStorage.setItem('todos',JSON.stringify(todos));
+
+
+}
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck)
+todoList.addEventListener('click', deleteCheck);
+document.addEventListener('DOMContentLoaded', getTodos)
 
 
